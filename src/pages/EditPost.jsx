00c84e4a3c8f7f8ac63ba9562f784d790dba8a01
khanittha <khanittha@ianalysis.biz>
2023-05-19 16:24:49 +0700

    update

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -4,8 +4,35 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+const baseURL = "http://dev.opensource-technology.com:3000/api/posts/[post_id]"
 
 export default function NewPost() {
+
+  const [get, setGet] = useState([]);
+  const [title, setTitle] = useState();
+  const [content, setContent] = useState();
+
+  const handleSave = () => {
+    axios
+      .post(
+        baseURL,
+        {
+          title: title,
+          content: content,
+          published: false,
+        },
+      )
+      .then((response) => {
+        setGet(response.data);
+        console.log(get);
+      })
+      .catch((e) => console.log(e));
+  };
+
+
   return (
     <>
       <Container>
@@ -16,19 +43,19 @@ export default function NewPost() {
             </Form.Label>{" "}
             <br />
             <Form.Label>Title</Form.Label>
-            <Form.Control placeholder="title" />
+            <Form.Control placeholder="title" onChange={(e) => setTitle(e.target.value)}/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Content</Form.Label>
-            <Form.Control placeholder="Content" />
+            <Form.Control placeholder="Content" onChange={(e) => setContent(e.target.value)}/>
           </Form.Group>
 
           <Form.Group className="mt-5 mb-2">
             <hr />
             <Row>
               <Col>
-                <Button variant="success">Save</Button>&nbsp;
+                <Button variant="success" onClick={handleSave}>Save</Button>&nbsp;
                 <Link to="/Posts">
                   <Button variant="secondary">Cancel</Button>
                 </Link>
